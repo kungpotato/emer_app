d31766aaf9f36259621e762026001d9a3593304d
KUNGPOTATO\kungp <kittisak.py@gmail.com>
2023-11-10 07:12:24 +0700

    change theme

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -9,12 +9,14 @@ import 'package:emer_app/app/authentication/store/auth_store.dart';
 import 'package:emer_app/app/locale/locale_store.dart';
 import 'package:emer_app/app/storage/theme_storeage.dart';
 import 'package:emer_app/app/theme/app_theme.dart';
+import 'package:emer_app/core/exceptions/app_error_hadler.dart';
 import 'package:emer_app/l10n/l10n.dart';
 import 'package:emer_app/pages/no_internet_page.dart';
 import 'package:emer_app/pages/pin_verify_page.dart';
 import 'package:emer_app/pages/user_profile_form_page.dart';
 import 'package:emer_app/pages/verify_email_page.dart';
 import 'package:emer_app/shared/extensions/context_extension.dart';
+import 'package:emer_app/shared/helper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -91,55 +93,65 @@ class AppState extends State<App> {
       }
     }).listen((user) {
       //
+    }, onError: (dynamic err) {
+      showSnack(context, text: err.toString());
+      handleError(err);
     });
   }
 
   void watchAuthState() {
     final authStore = Provider.of<AuthStore>(context, listen: false);
-    disposer = reaction((_) => authStore.status, (status) {
-      if (status == AuthStatus.noInternet) {
-        _navigator.pushAndRemoveUntil(
-          MaterialPageRoute<void>(
-            builder: (context) => const NoInternetPage(),
-          ),
-          (route) => false,
-        );
-      } else if (status == AuthStatus.unauthenticated) {
-        _navigator.pushAndRemoveUntil(
-          MaterialPageRoute<dynamic>(
-            builder: (context) => const LoginPage(),
-          ),
-          (route) => false,
-        );
-      } else if (status == AuthStatus.userInfo) {
-        _navigator.pushAndRemoveUntil(
-          MaterialPageRoute<void>(
-            builder: (context) => const UserProfileFormPage(),
-          ),
-          (route) => false,
-        );
-      } else if (status == AuthStatus.verify) {
-        _navigator.push(
-          MaterialPageRoute<void>(
-            builder: (context) => const PinVerifyPage(),
-          ),
-        );
-      } else if (status == AuthStatus.verifyEmail) {
-        _navigator.pushAndRemoveUntil(
-          MaterialPageRoute<dynamic>(
-            builder: (context) => VerifyEmailPage(),
-          ),
-          (route) => false,
-        );
-      } else if (status == AuthStatus.authenticated) {
-        _navigator.pushAndRemoveUntil(
-          MaterialPageRoute<dynamic>(
-            builder: (context) => const AppHome(),
-          ),
-          (route) => false,
-        );
-      }
-    });
+    disposer = reaction(
+      (_) => authStore.status,
+      (status) {
+        if (status == AuthStatus.noInternet) {
+          _navigator.pushAndRemoveUntil(
+            MaterialPageRoute<void>(
+              builder: (context) => const NoInternetPage(),
+            ),
+            (route) => false,
+          );
+        } else if (status == AuthStatus.unauthenticated) {
+          _navigator.pushAndRemoveUntil(
+            MaterialPageRoute<dynamic>(
+              builder: (context) => const LoginPage(),
+            ),
+            (route) => false,
+          );
+        } else if (status == AuthStatus.userInfo) {
+          _navigator.pushAndRemoveUntil(
+            MaterialPageRoute<void>(
+              builder: (context) => const UserProfileFormPage(),
+            ),
+            (route) => false,
+          );
+        } else if (status == AuthStatus.verify) {
+          _navigator.push(
+            MaterialPageRoute<void>(
+              builder: (context) => const PinVerifyPage(),
+            ),
+          );
+        } else if (status == AuthStatus.verifyEmail) {
+          _navigator.pushAndRemoveUntil(
+            MaterialPageRoute<dynamic>(
+              builder: (context) => VerifyEmailPage(),
+            ),
+            (route) => false,
+          );
+        } else if (status == AuthStatus.authenticated) {
+          _navigator.pushAndRemoveUntil(
+            MaterialPageRoute<dynamic>(
+              builder: (context) => const AppHome(),
+            ),
+            (route) => false,
+          );
+        }
+      },
+      onError: (err, _) {
+        handleError(err);
+        showSnack(context, text: err.toString());
+      },
+    );
   }
 
   @override
