057eabba6d196ed0873e9afbcb5a3eba91861422
kungpotato <kittisak_p@scasset.com>
2023-11-23 13:31:09 +0700

    init

--- lib/pages/call_process_page.dart
+++ lib/pages/call_process_page.dart
@@ -10,6 +10,10 @@ class CallProcessPage extends StatefulWidget {
 class _CallProcessPageState extends State<CallProcessPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(children: []),
+      ),
+    );
   }
 }
